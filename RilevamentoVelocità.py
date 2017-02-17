0edf1ccc361d2482bc4bafc29484bc2df6c26051
crivellarodiego82 <crivellarodiego.82@gmail.com>
2017-02-17 08:11:07 +0100

    Create RilevamentoVelocità.py

---
+++ RilevamentoVelocità.py
@@ -0,0 +1,166 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import math
+import datetime
+import cv2
+
+# place a prompt on the displayed image
+def prompt_on_image(txt):
+    global image
+    cv2.putText(image, txt, (10, 35),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+     
+# calculate speed from pixels and time
+def get_speed(pixels, ftperpixel, secs):
+    if secs > 0.0:
+        return ((pixels * ftperpixel)/ secs) * 0.681818  
+    else:
+        return 0.0
+ 
+# calculate elapsed seconds
+def secs_diff(endTime, begTime):
+    diff = (endTime - begTime).total_seconds()
+    return diff    
+
+# mouse callback function for drawing capture area
+def draw_rectangle(event,x,y,flags,param):
+    global ix,iy,fx,fy,drawing,setup_complete,image, org_image, prompt
+ 
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+ 
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            image = org_image.copy()
+            prompt_on_image(prompt)
+            cv2.rectangle(image,(ix,iy),(x,y),(0,255,0),2)
+  
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        fx,fy = x,y
+        image = org_image.copy()
+        prompt_on_image(prompt)
+        cv2.rectangle(image,(ix,iy),(fx,fy),(0,255,0),2)
+        
+# define some constants
+DISTANCE = 76  #<---- enter your distance-to-road value here
+THRESHOLD = 15
+MIN_AREA = 175
+BLURSIZE = (15,15)
+IMAGEWIDTH = 640
+IMAGEHEIGHT = 480
+RESOLUTION = [IMAGEWIDTH,IMAGEHEIGHT]
+FOV = 53.5
+FPS = 30
+
+# the following enumerated values are used to make the program more readable
+WAITING = 0
+TRACKING = 1
+SAVING = 2
+UNKNOWN = 0
+LEFT_TO_RIGHT = 1
+RIGHT_TO_LEFT = 2
+
+# calculate the the width of the image at the distance specified
+frame_width_ft = 2*(math.tan(math.radians(FOV*0.5))*DISTANCE)
+ftperpixel = frame_width_ft / float(IMAGEWIDTH)
+print("Image width in feet {} at {} from camera".format("%.0f" % frame_width_ft,"%.0f" % DISTANCE))
+
+# state maintains the state of the speed computation process
+# if starts as WAITING
+# the first motion detected sets it to TRACKING
+ 
+# if it is tracking and no motion is found or the x value moves
+# out of bounds, state is set to SAVING and the speed of the object
+# is calculated
+# initial_x holds the x value when motion was first detected
+# last_x holds the last x value before tracking was was halted
+# depending upon the direction of travel, the front of the
+# vehicle is either at x, or at x+w 
+# (tracking_end_time - tracking_start_time) is the elapsed time
+# from these the speed is calculated and displayed 
+ 
+state = WAITING
+direction = UNKNOWN
+initial_x = 0
+last_x = 0
+ 
+#-- other values used in program
+base_image = None
+abs_chg = 0
+mph = 0
+secs = 0.0
+show_bounds = True
+showImage = True
+ix,iy = -1,-1
+fx,fy = -1,-1
+drawing = False
+setup_complete = False
+tracking = False
+text_on_image = 'No cars'
+loop_count = 0
+prompt = ''
+
+# initialize the camera 
+camera = PiCamera()
+camera.resolution = RESOLUTION
+camera.framerate = FPS
+camera.vflip = False
+camera.hflip = False
+
+rawCapture = PiRGBArray(camera, size=camera.resolution)
+# allow the camera to warm up
+time.sleep(0.9)
+
+# create an image window and place it in the upper left corner of the screen
+cv2.namedWindow("Speed Camera")
+cv2.moveWindow("Speed Camera", 10, 40)
+
+# call the draw_rectangle routines when the mouse is used
+cv2.setMouseCallback('Speed Camera',draw_rectangle)
+ 
+# grab a reference image to use for drawing the monitored area's boundry
+camera.capture(rawCapture, format="bgr", use_video_port=True)
+image = rawCapture.array
+rawCapture.truncate(0)
+org_image = image.copy()
+
+prompt = "Define the monitored area - press 'c' to continue" 
+prompt_on_image(prompt)
+ 
+# wait while the user draws the monitored area's boundry
+while not setup_complete:
+    cv2.imshow("Speed Camera",image)
+ 
+    #wait for for c to be pressed  
+    key = cv2.waitKey(1) & 0xFF
+  
+    # if the `c` key is pressed, break from the loop
+    if key == ord("c"):
+        break
+
+# the monitored area is defined, time to move on
+prompt = "Press 'q' to quit" 
+ 
+# since the monitored area's bounding box could be drawn starting 
+# from any corner, normalize the coordinates
+ 
+if fx > ix:
+    upper_left_x = ix
+    lower_right_x = fx
+else:
+    upper_left_x = fx
+    lower_right_x = ix
+ 
+if fy > iy:
+    upper_left_y = iy
+    lower_right_y = fy
+else:
+    upper_left_y = fy
+    lower_right_y = iy
+     
+monitored_width = lower_right_x - upper_left_x
+monitored_height = lower_right_y - upper_left_y
